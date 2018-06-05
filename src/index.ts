94fc0cf72034252afeec3d76f2a3a0eb719cfcd3
cthos <daginus@gmail.com>
2018-06-04 18:40:51 -0700

    Fix Some authentication errors.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as Octokit from '@octokit/rest';
 import * as moment from 'moment';
+import * as HTTPError from 'http-errors';
 
 interface IGithubConfig {
   org?: string;
@@ -22,6 +23,8 @@ class VerdaccioGithubAuth {
   private cachettl: number;
 
   private octokit: Octokit;
+
+  private badUsers: any[] = [];
   
   // TODO: hook into a not-in-memory cache
   private teamCache: any = {};
@@ -44,21 +47,57 @@ class VerdaccioGithubAuth {
    * @param callback 
    */
   public async authenticate(username, password, callback): Promise<void> {
+    // Don't try to get teams if the user's auth is bad.
+    if (this.badUsers.indexOf(username) > -1) {
+      return callback(HTTPError(403, 'Bad Username/Password.'));
+    }
+
+    this.githubAuth(username, password);
+
+    const teams = await this.getUserTeams(username);
+    return callback(null, teams);
+  }
+
+  /**
+   * Verify that the user is able to be authenticated at github.
+   * 
+   * @param username 
+   * @param password 
+   * @param callback 
+   */
+  public async adduser(username, password, callback): Promise<void> {
+    this.githubAuth(username, password);
+
+    // Check to ensure we can get a user:
+    this.octokit.users.get({})
+      .then((resp) => {
+        this.badUsers = this.badUsers.filter(u => u != username);
+        return callback(null, true);
+      })
+      .catch(err => {
+        // Cache the bad user list so we don't later make an extraneous call during auth
+        this.badUsers.push(username);
+        return callback(HTTPError(409, 'Bad Username/Password'), false);
+      });
+  }
+
+  private githubAuth(username, password) {
     if (this.mode === 'token') {
       this.octokit.authenticate({
         type: 'token',
         token: password
       });
+      return;
     } else if (this.mode === 'basic') {
       this.octokit.authenticate({
         type: 'basic',
         username: username,
         password: password
       });
+      return;
     }
     
-    const teams = await this.getUserTeams(username);
-    callback(null, teams);
+    throw new Error('Unsupported authentication type in Verdaccio config.');
   }
 
   /**
